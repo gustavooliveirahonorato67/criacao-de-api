afc6e7a49a89c058920f14989c45fb5b10161fb8
gustavooliveirahonorato67 <guhonorato25@Gmail.com>
2026-03-13 11:59:33 +0000

    ajustando o codigo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const servidor = express();
 
-servidor.get("/Gustavo67") (req,res) => {
+servidor.get("/Gustavo67", (req,res) => {
 })
 
 servidor.listen(3000,() => {
