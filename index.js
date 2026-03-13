985ccaea18c4ecfc1b667cf77e31b177dd563f2e
gustavooliveirahonorato67 <guhonorato25@Gmail.com>
2026-03-13 11:58:50 +0000

    commit da atividade

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const servidor = express();
+
+servidor.get("/Gustavo67") (req,res) => {
+})
+
+servidor.listen(3000,() => {
+    console.log('notebook a 67 fps btw')
+})
